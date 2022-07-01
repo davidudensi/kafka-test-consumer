8ca5928a6e631c8f652ed54a88387f335e39d34d
davidudensi <david.udensi@jdplc.com>
2022-07-01 23:22:40 +0100

    first commit

---
+++ KafkaTestConsumer/Program.cs
@@ -0,0 +1,26 @@
+﻿using Confluent.Kafka;
+
+namespace KafkaTestConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConsumerConfig
+            {
+                GroupId = "gid-consumers",
+                BootstrapServers = "localhost:9092"
+            };
+
+            using(var consumer = new ConsumerBuilder<Null, string>(config).Build())
+            {
+                consumer.Subscribe("TestTopic");
+                while (true)
+                {
+                    var record = consumer.Consume();
+                    Console.WriteLine(record.Message.Value);
+                }
+            }
+        }
+    }
+}
